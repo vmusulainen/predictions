9c176a2d25fe024254539f638224712c42f79873
vmusulainen <vmusulainen@gmail.com>
2015-05-28 11:02:37 +0300

    Prediction market depth showed closed orders - it's fixed

--- client/views/prediction/predictionMarketDepth/predictionMarketDepth.js
+++ client/views/prediction/predictionMarketDepth/predictionMarketDepth.js
@@ -1,8 +1,16 @@
 Template.predictionMarketDepth.helpers({
     sellOrders: function () {
-        return Orders.find({predictionID: this._id, type: 'sell'}, {sort: {price: 1}})
+        return Orders.find({
+            predictionID: this._id,
+            type: Constants.Order.Types.sell,
+            state: Constants.Order.States.opened
+        }, {sort: {price: 1}})
     },
     buyOrders: function () {
-        return Orders.find({predictionID: this._id, type: 'buy'}, {sort: {price: -1}})
+        return Orders.find({
+            predictionID: this._id,
+            type: Constants.Order.Types.buy,
+            state: Constants.Order.States.opened
+        }, {sort: {price: -1}})
     }
 });
