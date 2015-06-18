e97d1509e41962438d22ff46a1a97ba442a5d7d5
vmusulainen <vmusulainen@gmail.com>
2015-06-19 02:44:28 +0300

    Validation for quantity and price fields was added

--- client/views/prediction/predictionTrade/predictionTrade.js
+++ client/views/prediction/predictionTrade/predictionTrade.js
@@ -1,6 +1,10 @@
 Template.predictionTrade.helpers({
     hasOpenedOrders: function () {
-        return Orders.find({predictionID: this._id, userID: Meteor.userId(), state: Constants.Order.States.opened}).count() != 0;
+        return Orders.find({
+                predictionID: this._id,
+                userID: Meteor.userId(),
+                state: Constants.Order.States.opened
+            }).count() != 0;
     },
 
     openedOrders: function () {
@@ -10,19 +14,50 @@ Template.predictionTrade.helpers({
 
 
 Template.predictionTrade.events({
-    'click #sellButton': function (event, ui) {
+
+    "keyup form": function (event, ui) {
+        var quantityValue = $(event.target).closest('form').find('.quantity').val();
+        var priceValue = $(event.target).closest('form').find('.price').val();
+        var errorDisplayingElement = $(event.target).closest('div.well').find('.validation-error');
+        var buttonElement = $(event.target).closest('form').find('button.btn');
+        validateInput(quantityValue, priceValue, errorDisplayingElement, buttonElement);
+    },
+
+
+    "keyup .price": function (event, ui) {
+        var inputElement = $(event.target).closest('form').find('.price');
+
+        var errorDisplayingElement = $(event.target).closest('div.well').find('.validation-error');
+        var buttonElement = $(event.target).closest('form').find('button.btn');
+
+        var value = inputElement.val();
+        var qty = Number(value);
+
+        if ((value != '') && ((isNaN(qty) || (qty <= 0)))) {
+            errorDisplayingElement.removeClass('invisible');
+            errorDisplayingElement.text(i18n("The price must be between 0 and 1.0"));
+            buttonElement.addClass('disabled');
+        } else {
+            errorDisplayingElement.addClass('invisible');
+            buttonElement.removeClass('disabled');
+            errorDisplayingElement.text("&nbsp");
+        }
+    },
+
+    'click button.sell': function (event, ui) {
         var user = Meteor.user();
         var prediction = Predictions.findOne({_id: this._id});
-        var qty = Number($(ui.find('#sellQuantity')).val());
-        var price = Number($(ui.find('#sellPrice')).val());
+        var qty = Number($(ui.find('form.sell input.quantity')).val());
+        var price = Number($(ui.find('form.sell input.price')).val());
         sellDialog(user, prediction, qty, price);
         return false
     },
-    'click #buyButton': function (event, ui) {
+
+    'click button.buy': function (event, ui) {
         var user = Meteor.user();
         var prediction = Predictions.findOne({_id: this._id});
-        var qty = Number($(ui.find('#buyQuantity')).val());
-        var price = Number($(ui.find('#buyPrice')).val());
+        var qty = Number($(ui.find('form.buy input.quantity')).val());
+        var price = Number($(ui.find('form.buy input.price')).val());
         buyDialog(user, prediction, qty, price);
         return false
     }
@@ -95,4 +130,48 @@ var buyDialog = function (user, prediction, qty, price) {
             }
         }
     })
+};
+
+var validateInput = function (qtyValue, priceValue, errorElement, button) {
+
+    var canPressButton = true;
+
+    if ((qtyValue === '') && (priceValue === '')) {
+        errorElement.addClass('invisible');
+        errorElement.text("&nbsp");
+        button.addClass('disabled');
+        return;
+    }
+
+    if (qtyValue != '') {
+        var qty = Number(qtyValue);
+        if ((isNaN(qty) || (qty <= 0))) {
+            errorElement.removeClass('invisible');
+            errorElement.text(i18n("The quantity must be an integer greater than 0"));
+            button.addClass('disabled');
+            return;
+        }
+    }
+    else {
+        canPressButton = false;
+    }
+
+    if (priceValue != '') {
+        var price = Number(priceValue);
+        if ((isNaN(price) || (price <= 0) || (price > 1))) {
+            errorElement.removeClass('invisible');
+            errorElement.text(i18n("The price must be between 0 and 1.0"));
+            button.addClass('disabled');
+            return;
+        }
+    }
+    else {
+        canPressButton = false;
+    }
+
+    errorElement.addClass('invisible');
+    errorElement.text("&nbsp");
+
+    canPressButton === true ? button.removeClass('disabled') : button.addClass('disabled');
+
 };
