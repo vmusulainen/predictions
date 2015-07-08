1421c2cb455d73597bee876e1ff63ebc4fd82446
Vladimir Musulainen <vmusulainen@gmail.com>
2015-07-09 02:25:58 +0300

    https://github.com/vmusulainen/predictions/issues/8
    Start page now shows all predictions without grouping by events

--- client/views/events/events.js
+++ client/views/events/events.js
@@ -1,22 +1,7 @@
 Template.eventList.helpers({
-    events: function () {
-        return Events.find()
-    },
     predictions: function () {
-        return Predictions.find({eventID: Session.get('selectedEventID')});
-    },
-    predictionCount: function () {
-        return Predictions.find({eventID: this._id}).count()
-    },
-    isActive: function () {
-        return this._id == Session.get('selectedEventID');
+        return Predictions.find();
     }
 
 });
-
-Template.eventList.events({
-    'click .event': function () {
-        Session.set('selectedEventID', this._id);
-    }
-})
 
